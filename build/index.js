03ff4ff54f60f7b9144924e9065c821f76865443
carolcampos22 <mendes.carolina2209@gmail.com>
2023-07-17 17:18:14 -0300

    projeto concluído

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./database");
-console.log(database_1.users);
-console.log(database_1.products);
-console.log((0, database_1.createUser)("03", "Sicrano", "sicrano@gmail.com", "sicsic123"));
-console.table(database_1.users);
-console.log("All users: ", (0, database_1.getAllUsers)());
-//# sourceMappingURL=index.js.map
